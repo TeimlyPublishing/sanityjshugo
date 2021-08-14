5a99d719485e30afa9a66f164203bddfc8e59195
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-14 03:03:04 -0400

    production-2

---
+++ helloworld/schemas/post.js
@@ -0,0 +1,50 @@
+export default {
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [{
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [{ type: 'reference', to: { type: 'category' } }],
+        },
+        {
+            name: 'date',
+            title: 'Published at',
+            type: 'datetime',
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+        },
+    ],
+
+    preview: {
+        select: {
+            title: 'title',
+            author: 'author.name',
+            media: 'mainImage',
+        },
+        prepare(selection) {
+            const { author } = selection
+            return Object.assign({}, selection, {
+                subtitle: author && `by ${author}`,
+            })
+        },
+    },
+}
